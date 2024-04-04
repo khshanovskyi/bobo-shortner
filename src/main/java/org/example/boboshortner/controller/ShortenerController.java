06e270475e265cfbf3f3bacc0d3ddb8155ac38d7
khshanovskyi <Pavlo_Khshanovskyi@epam.com>
2024-04-04 10:26:16 +0300

    Fix NPE

--- src/main/java/org/example/boboshortner/controller/ShortenerController.java
+++ src/main/java/org/example/boboshortner/controller/ShortenerController.java
@@ -3,12 +3,14 @@ package org.example.boboshortner.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.boboshortner.model.Url;
 import org.example.boboshortner.service.ShortenerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -17,14 +19,14 @@ public class ShortenerController {
 
     private final ShortenerService shortenerService;
 
-    @GetMapping("/")
+    @GetMapping
     public String displayForm(Model model) {
         model.addAttribute("originalUrl", "");
         model.addAttribute("shortenedUrl", "");
         return "shortener";
     }
 
-    @PostMapping("/")
+    @PostMapping
     public String shortenUrl(@RequestParam String originalUrl, Model model) {
         Url url = shortenerService.createShortUrl(originalUrl);
         String shortUrl = url.getShortUrl();
@@ -35,10 +37,18 @@ public class ShortenerController {
 
     @GetMapping(path = "/{key}")
     public ResponseEntity<?> redirectToSpecificURL(@PathVariable String key) {
-        String originalUrl = shortenerService.getOriginalUrl(key);
-        return ResponseEntity.status(302)
-                .header("Location", originalUrl)
-                .build();
+        try {
+            String originalUrl = shortenerService.getOriginalUrl(key);
+            if (originalUrl == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("URL not found");
+            }
+            return ResponseEntity.status(302)
+                    .header("Location", originalUrl)
+                    .build();
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while redirecting");
+        }
     }
 
 }

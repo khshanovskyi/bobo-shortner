425edff09ea203e3918f1022178ea94d368d5a0a
Mykola Klushyn <mklushyn@atlassian.com>
2024-04-04 06:50:58 +0200

    first commit

---
+++ src/main/java/org/example/boboshortner/BoboShortnerApplication.java
@@ -0,0 +1,13 @@
+package org.example.boboshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoboShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoboShortnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/boboshortner/BoboShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boboshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoboShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
